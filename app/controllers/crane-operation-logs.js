7cf48aaa41cef956764d29fc8c29d96d2b83f5ca
Long Ho <longxuanho@gmail.com>
2017-03-21 08:19:40 +0700

    adding get request

--- app/controllers/crane-operation-logs.js
+++ app/controllers/crane-operation-logs.js
@@ -5,13 +5,24 @@ module.exports = function (app, admin) {
   app.use('/api/sts-operation-logs', router);
 
   router
+    .get('/', function (req, res, next) {
+      console.log(req.query);
+      if (req.query.message && typeof(req.query.message) === "string") {
+        admin.database().ref('sts-operation-logs').push(req.query.message);
+        res.status(200).end();
+      }
+      else {
+        console.log('validating data failed');
+        res.status(400).send('Validating model failed');
+      }
+    })
     .post('/', function (req, res, next) {
       if (req.body && typeof(req.body) === "string") {
         admin.database().ref('sts-operation-logs').push(req.body);
         res.status(200).end();
         console.log(req.body);
       } else {
-        console.log('validating data failed', newLog);
+        console.log('validating data failed');
         res.status(400).send('Validating model failed');
       }
     });

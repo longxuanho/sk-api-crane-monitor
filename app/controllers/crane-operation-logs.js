bdfc8895582b202a5acaa68470af4cb74e6fbec7
Long Ho <longxuanho@gmail.com>
2017-03-30 08:15:13 +0700

    update new query

--- app/controllers/crane-operation-logs.js
+++ app/controllers/crane-operation-logs.js
@@ -7,21 +7,20 @@ module.exports = function (app, admin) {
   router
     .get('/', function (req, res, next) {
       console.log(req.query);
-      if (req.query.message && typeof(req.query.message) === "string") {
-        admin.database().ref('sts-operation-logs').push(req.query.message);
+      if (req.query) {
+        var preparedData = {
+          asset: req.query.asset,
+          contNum: req.query.contNum,
+          category: req.query.category,
+          start: req.query.start,
+          end: req.query.end
+        }
+
+        admin.database().ref('sts-operation-logs').push(preparedData);
         res.status(200).end();
       }
+
       else {
-        console.log('validating data failed');
-        res.status(400).send('Validating model failed');
-      }
-    })
-    .post('/', function (req, res, next) {
-      if (req.body && typeof(req.body) === "string") {
-        admin.database().ref('sts-operation-logs').push(req.body);
-        res.status(200).end();
-        console.log(req.body);
-      } else {
         console.log('validating data failed');
         res.status(400).send('Validating model failed');
       }
